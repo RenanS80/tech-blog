af59a488a2984aa278beee919b7fa412d74228e5
Renan Soares <renansoaresffc@gmail.com>
2022-01-23 16:21:12 -0300

    Responsive and ScrollReveal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+// Form Mask
 $(document).ready(function(){
      $('.telefone').mask('(00) 00000-0000');
 });
+
+
+// Scroll Reveal
+ScrollReveal().reveal('.card', { interval: 200 });
+ScrollReveal().reveal('form', { delay: 200 });
+
