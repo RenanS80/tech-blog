bd36a78e19b20b83ec58e7196a48f581a1e3ea6e
Renan Soares <renansoaresffc@gmail.com>
2023-12-27 12:52:50 -0300

    feat: refatoração para nova UI do Figma

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-// Form Mask
-$(document).ready(function(){
-     $('.telefone').mask('(00) 00000-0000');
-});
-
-
 // Scroll Reveal
 ScrollReveal().reveal('.card', { interval: 200 });
 ScrollReveal().reveal('form', { delay: 200 });
